713cf1c5b1098d5c24a2b4c1b9b116cb393144b2
Jose Casanova <josescasanova@gmail.com>
2014-02-25 09:50:03 -0500

    fixing errors 2

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,7 +9,7 @@ class Post < ActiveRecord::Base
   validates :description, presence: true
   validates :url, presence: true, uniqueness: true
 
-  before_save :generate_slug
+  before_save :generate_slug!
 
   def total_votes
      self.up_votes - self.down_votes 
@@ -27,9 +27,8 @@ class Post < ActiveRecord::Base
     self.slug
   end
 
- def generate_slug
+ def generate_slug!
    self.slug = self.title.gsub(" ", "-").downcase
-   self.save
  end
 
 end
